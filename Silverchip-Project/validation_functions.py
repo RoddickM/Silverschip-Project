f7314c7cba330ee49a69578097d33d04a5a653ce
RoddickM <98747226+RoddickM@users.noreply.github.com>
2022-01-31 14:58:13 +0000

    Update validation_functions.py

--- Silverchip-Project/validation_functions.py
+++ Silverchip-Project/validation_functions.py
@@ -8,7 +8,16 @@ def float_validation(text):
 
 def list_answers(text, list_of_answers):
     user_input = input(text)
+    if user_input in list_of_answers:
+        return user_input
+    else:
+        print(f"\nPlease only enter these values: {list_of_answers}\n")
 
 
-def range_accept():
-    pass
+def float_range_accept(text, start_num, end_num):
+    user_input = float_validation(text)
+    if user_input in range(start_num, end_num+1):
+        return user_input
+    else:
+        print(f"\nPlease enter a number between the range of {start_num} and {end_num+1}\n")
+
